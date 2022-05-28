e5efae50999e911e3a3e752072a9646225607494
JonatanMGit <60609358+JonatanMGit@users.noreply.github.com>
2022-05-29 00:01:59 +0200

    the array

--- src/index.js
+++ src/index.js
@@ -14,10 +14,6 @@ function sleep(time) {
   });
 }
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -39,7 +35,41 @@ client.on('ready', () => {
   })
   // grab a screenshot and send it to a discord channel with the id 799701208607752194
   let screenshot = await page.screenshot();
-  await browser.close();
+
+  // grab the text from #menuorder > tbody > tr:nth-child(1) > th:nth-child(2) > div and #menuorder > tbody > tr:nth-child(1) > th:nth-child(3) > div
+  let text = await page.evaluate(() => {
+
+    // repeat for each day of the week
+    var text = []
+    for (let i = 2; i <= 6; i++) {
+      text.push([document.querySelector('#menuorder > tbody > tr:nth-child(1) > th:nth-child(' + i + ') > div').innerText.replace(/[\n\r]/g, '')]);
+    }
+    // add a array to each array element
+    for (let i = 0; i < text.length; i++) {
+      text[i].push([])
+    }
+
+    for (let day = 2; day <= 6; day++) {
+      for (let meal = 2; meal <= 4; meal++) {
+        try {
+          text[day - 2][1].push(document.querySelector('#menuorder > tbody > tr:nth-child(' + meal + ') > td:nth-child(' + day + ') > div > span.description').innerText.replace(/[\n\r]/g, ''));
+        } catch (e) {
+          text[day - 2][1].push("");
+        }
+      }
+    }
+
+
+
+    return text;
+  });
+
+
+  console.log(text);
+
+
+
+  // await browser.close();
   // send the screenshot to the discord channel
   // client.channels.cache.get('799701208607752194').send({ files: [{ attachment: screenshot, name: 'peepee.png' }] });
 })();
